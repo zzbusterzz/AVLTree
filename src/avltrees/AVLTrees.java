d501e0b778673ac08d2b3fe40c722393b0e5c99d
Student <Student@DESKTOP-UU6MAVC>
2019-08-19 12:30:56 +0530

    First Commit on AVL

---
+++ src/avltrees/AVLTrees.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avltrees;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class AVLTrees {
+
+    public class Link
+    {
+        public int data;
+        public Link nextLeft;
+        public Link nextRight;
+        
+        
+    }
+    
+    
+    Scanner sc;
+    Link BeginNode;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        
+        AVLTrees instance = new AVLTrees();
+    }
+    
+    public AVLTrees()
+    {
+        
+    }
+    
+    public void Init()
+    {
+        
+    }
+    
+}
